9a0208c840bd6c7b65d581c5cf5165dc4e4cf83f
micalicious <dpdedios2030@clc.pshs.edu.ph>
2026-08-16 07:21:47 +0800

    Refactor zodiac sign calculation for clarity

--- q1/q1_sq2_a3.py
+++ q1/q1_sq2_a3.py
@@ -1,31 +1,23 @@
-birthyear = int(input("Enter your birth year: "))
+birth_year = int(input("Enter your birth year: "))
 
-if birthyear < 1900:
+if birth_year < 1900:
     print("Invalid Year, it should not be earlier than 1900")
 else:
-    zodiac = (birthyear - 1900) % 12
+    zodiac = [
+        "Rat (鼠 / Shǔ)",
+        "Ox (牛 / Niú)",
+        "Tiger (虎 / Hǔ)",
+        "Rabbit (兔 / Tù)",
+        "Dragon (龙 / Lóng)",
+        "Snake (蛇 / Shé)",
+        "Horse (马 / Mǎ)",
+        "Goat (羊 / Yáng)",
+        "Monkey (猴 / Hóu)",
+        "Rooster (鸡 / Jī)",
+        "Dog (狗 / Gǒu)",
+        "Pig (猪 / Zhū)"
+    ]
 
-    if zodiac == 0:
-        print("Your Chinese Zodiac Sign is : Rat (鼠 / Shǔ)")
-    elif zodiac == 1:
-        print("Your Chinese Zodiac Sign is : Ox (牛 / Niú)")
-    elif zodiac == 2:
-        print("Your Chinese Zodiac Sign is : Tiger (虎 / Hǔ)")
-    elif zodiac == 3:
-        print("Your Chinese Zodiac Sign is : Rabbit (兔 / Tù)")
-    elif zodiac == 4:
-        print("Your Chinese Zodiac Sign is : Dragon (龙 / Lóng)")
-    elif zodiac == 5:
-        print("Your Chinese Zodiac Sign is : Snake (蛇 / Shé)")
-    elif zodiac == 6:
-        print("Your Chinese Zodiac Sign is : Horse (马 / Mǎ)")
-    elif zodiac == 7:
-        print("Your Chinese Zodiac Sign is : Goat (羊 / Yáng)")
-    elif zodiac == 8:
-        print("Your Chinese Zodiac Sign is : Monkey (猴 / Hóu)")
-    elif zodiac == 9:
-        print("Your Chinese Zodiac Sign is : Rooster (鸡 / Jī)")
-    elif zodiac == 10:
-        print("Your Chinese Zodiac Sign is : Dog (狗 / Gǒu)")
-    elif zodiac == 11:
-        print("Your Chinese Zodiac Sign is : Pig (猪 / Zhū)")
+    index = (birth_year - 1900) % 12
+
+    print("Your Chinese Zodiac Sign is :", zodiac[index])
